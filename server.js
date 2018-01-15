1666b47341a5e5f8464ad903a174d700319f1919
zqler <434561406@qq.com>
2018-01-15 20:23:23 +0800

    修改webpack-dev-server配置

--- server.js
+++ server.js
@@ -6,8 +6,26 @@ new WebpackDevServer(webpack(config), {
     hot: true,
     inline: true,
     progress: true,
+    compress: true,
+    quiet: false,
+    noInfo: false,
+    open: true,
+    lazy: false,
+    stats: { colors: true },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    },
+    before: function(app) {
+        // Here you can access the Express app object and add your own custom middleware to it.
+        // For example, to define custom handlers for some paths:
+        // app.get('/some/path', function(req, res) {
+        //   res.json({ custom: 'response' });
+        // });
+    },
     historyApiFallback: true,
     disableHostCheck: true
+        //设置反向代理实现跨域
         // proxy: {
         //     '/api': {    //项目可以用的接口
         //         target: '******',//代理配置接口地址
